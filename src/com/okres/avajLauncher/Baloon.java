e56898f41d3dddcf551e43c568cd1f24a103cf25
SifuUa <alekseykres@gmail.com>
2018-03-31 23:53:04 +0300

    begin do output

--- src/com/okres/avajLauncher/Baloon.java
+++ src/com/okres/avajLauncher/Baloon.java
@@ -14,6 +14,7 @@ public class Baloon extends Aircraft implements Flyable {
         if (weather.equals("SUN")) {
             coordinates.setLongitude(coordinates.getLongitude() + 2);
             coordinates.setHeight(coordinates.getHeight() + 4);
+            System.out.println("Ballon" + "#" + this.name + "(" + this.id + "): " + "Wow, such a cool weather!!!");
         } else if (weather.equals("RAIN")) {
             coordinates.setHeight(coordinates.getHeight() - 5);
         } else if (weather.equals("FOG")) {
