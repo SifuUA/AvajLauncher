7a297c7fa348c80fafeaa9f1847806fd48b4c446
SifuUa <alekseykres@gmail.com>
2018-04-02 23:15:22 +0300

    add Logger class

---
+++ src/com/okres/avajLauncher/Logger.java
@@ -0,0 +1,26 @@
+package com.okres.avajLauncher;
+
+public class Logger {
+
+    private static Logger logger;
+
+    private static String str;
+
+    private Logger() {
+    }
+
+    public static synchronized Logger getLogger() {
+        if (logger == null) {
+            logger = new Logger();
+        }
+        return logger;
+    }
+
+    public void loggerAddLog(String log) {
+        str +=  log + '\n';
+    }
+
+    public void loggerShowLog() {
+        System.out.println(str);
+    }
+}
