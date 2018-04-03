c7e6006a47a125a24a352b07cbef35f9961e271a
SifuUa <alekseykres@gmail.com>
2018-04-03 23:23:50 +0300

    add write log in file finished

--- src/com/okres/avajLauncher/Logger.java
+++ src/com/okres/avajLauncher/Logger.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Logger {
 
     private static Logger logger;
-    private static String str;
+    private static String str = "";
     private static final String FILENAME = "/home/okres/IdeaProjects/AvajLauncher/src/LogFile.txt";
 
     private Logger() {
@@ -34,6 +34,7 @@ public class Logger {
             FileWriter fileWriter = new FileWriter(file);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             bufferedWriter.write(str);
+            bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
